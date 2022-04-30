1516c7529037a0de03cdd483e4a8a8bd5ab93792
silviolima07 <silviolima07@gmail.com>
2022-04-30 15:33:49 -0300

    teste

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def main():
         #cidade = st.text_input('Informe uma cidade', help="formato cidade-uf")
         cidade = st.selectbox(
      'Escolha uma cidade',
-     df_cidades['cidade-uf'].upper())
+     (df_cidades['cidade-uf']).upper())
 
         #cidade = 'sao-paulo-sp'
         st.subheader(cidade.upper())
