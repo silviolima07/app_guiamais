28545746f36d33937aada2b989db6eb9ed5464f6
silviolima07 <silviolima07@gmail.com>
2022-04-30 23:22:27 -0300

    teste

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def main():
 
     if choice == activities[0]:
         st.subheader("Objetivo:")
-        st.markdown("### - descobrir o endereço")
+        st.markdown("### - descobrir o endereço de restaurantes na cidade")
         
        
         
@@ -163,13 +163,13 @@ def main():
         
         st.write("Dados coletados via scrap usando: Selenium e BeautifulSoup.")
         #st.markdown("A coleta dos dados é feita às 9h, 12h, 15h e 18h")
-        st.write("Executados via crontab scripts realizam o scrap e atualização do app.")
-        st.write("Foram definidos 4 cargos apenas para validar o processo.")
-        st.write("O scrap para o cargo de Engenheiro de Machine Learning trouxe poucas linhas.")
-        st.write("Para os demais cargos, foram encontradas mais de 100 vagas, distribuídas em diversas páginas.")
-        st.write("Esse app traz as 10 primeiras páginas apenas.")
-        st.subheader("Observacao:")
-        st.write("O codigo html da pagina muda ao longo do tempo e ajustes no scrap são necessarios.")
+        #st.write("Executados via crontab scripts realizam o scrap e atualização do app.")
+        #st.write("Foram definidos 4 cargos apenas para validar o processo.")
+        #st.write("O scrap para o cargo de Engenheiro de Machine Learning trouxe poucas linhas.")
+        #st.write("Para os demais cargos, foram encontradas mais de 100 vagas, distribuídas em diversas páginas.")
+        st.write("Esse app traz as 5 primeiras páginas apenas.")
+        #st.subheader("Observacao:")
+        #st.write("O codigo html da pagina muda ao longo do tempo e ajustes no scrap são necessarios.")
         #st.subheader("Versão 02")
         #st.write(" - incluído o link encurtado da vaga")
         st.subheader("by Silvio Lima")
